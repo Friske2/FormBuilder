ef54fa1d0ad32af15e5ce5487946cf6c51f493eb
jellopy <parnupat.j@gmail.com>
2025-05-31 16:32:30 +0700

    imp feature col

---
+++ src/components/FieldCol.vue
@@ -0,0 +1,11 @@
+<template>
+    <el-col :span="props.span">
+        <slot></slot>
+    </el-col>
+</template>
+<script lang="ts" setup>
+import { defineProps } from "vue";
+const props = defineProps<{
+    span: number
+}>();
+</script>
--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-form ref="warpElForm" :model="form" label-width="120px" label-position="left">
+  <el-form
+    ref="warpElForm"
+    :model="form"
+    label-width="120px"
+    label-position="left"
+  >
     <el-form-item
       :rules="field.rules"
       v-for="field in fields"
@@ -8,30 +13,57 @@
       :prop="field.code"
       :key="field.code"
     >
-      <FieldItem 
+      <template v-if="field.is == 'col'">
+        <FieldCol
+          v-for="col in field.children"
+          :key="col.code"
+          :span="col.span"
+        >
+          <field-span :style="col.props.style" v-if="col.is == 'span'" :content="col.props.content" />
+          <el-form-item
+            v-else
+            :rules="col.rules"
+            :prop="col.code"
+            :key="col.code"
+          >
+            <FieldItem
+              :value="form[col.code]"
+              @update:value="form[col.code] = $event"
+              :field="col"
+            />
+          </el-form-item>
+        </FieldCol>
+      </template>
+      <FieldItem
+        v-else
         :value="form[field.code]"
         @update:value="form[field.code] = $event"
-        :field="field" />
+        :field="field"
+      />
     </el-form-item>
   </el-form>
 </template>
 <script lang="ts" setup>
-import { reactive,ref } from "vue";
+import { reactive, ref } from "vue";
 import FieldItem from "./FieldItem.vue";
-import exampleForm from "../mocks/exampleForm.json"
+import exampleForm from "../mocks/exampleForm.json";
+import FieldCol from "./FieldCol.vue";
+import FieldSpan from "./fields/FieldSpan.vue";
 import type { Schema } from "../types/Schema";
 const form = reactive<Record<string, any>>({
   name: "Sample Value",
   region: null,
   resource: null,
   type: [],
-  date1: "2023-10-01",
+  date1: "",
   time1: null,
   desc: null,
   isActive: true,
   isAccepted: false,
 });
-const warpElForm = ref<InstanceType<typeof import("element-plus")["ElForm"]> | null>(null);
+const warpElForm = ref<InstanceType<
+  typeof import("element-plus")["ElForm"]
+> | null>(null);
 const submit = async () => {
   try {
     let payload = null;
@@ -53,5 +85,4 @@ defineExpose({
   submit,
 });
 const fields = reactive<Schema>(exampleForm);
-
 </script>
