7e479bcf6b1cd3cf72bc5b39f961d2806ddb8164
jellopy <parnupat.j@gmail.com>
2025-06-14 15:19:58 +0700

    Add credit card form structure and validation rules

--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -7,33 +7,35 @@
       <strong>Note:</strong> The form does not submit to any backend service, but you can use the <code>submit</code> method to get the form data.
     </div>
   </div>
-  <el-form ref="warpElForm" :model="form" v-bind="config">
-    <template v-for="field in warpField" :key="field.code">
-      <show-if-wrapper :showIf="field.showIf" :formData="form" >
-        <el-form-item :rules="field.rules" :label-postion="field.label.position" :label="field.label.text"
-          :prop="field.code">
-          <!-- render nasted field under col -->
-          <template v-if="field.is == 'col'">
-            <field-col v-for="col in field.children" :key="col.code" :span="col.span">
-              <field-span :style="col.props.style" v-if="col.is == 'span'" :content="col.props.content" />
-              <el-form-item v-else :rules="col.rules" :prop="col.code" :key="col.code">
-                <field-item :value="form[col.code]" @update:value="form[col.code] = $event" :field="col" />
-              </el-form-item>
-            </field-col>
-          </template>
-          <!-- render field item -->
-          <template v-else>
-            <field-item :value="form[field.code]" @update:value="form[field.code] = $event" :field="field" />
-          </template>
-        </el-form-item>
-      </show-if-wrapper>
-    </template>
-  </el-form>
+  <div style="width:500px">
+    <el-form ref="warpElForm" :model="form" v-bind="config">
+      <template v-for="field in warpField" :key="field.code">
+        <show-if-wrapper :showIf="field.showIf" :formData="form" >
+          <el-form-item :rules="field.rules" :label-postion="field.label.position" :label="field.label.text"
+            :prop="field.code">
+            <!-- render nasted field under col -->
+            <template v-if="field.is == 'col'">
+              <field-col v-for="col in field.children" :key="col.code" :span="col.span">
+                <field-span :style="col.props.style" v-if="col.is == 'span'" :content="col.props.content" />
+                <el-form-item v-else :rules="col.rules" :prop="col.code" :key="col.code">
+                  <field-item :value="form[col.code]" @update:value="form[col.code] = $event" :field="col" />
+                </el-form-item>
+              </field-col>
+            </template>
+            <!-- render field item -->
+            <template v-else>
+              <field-item :value="form[field.code]" @update:value="form[field.code] = $event" :field="field" />
+            </template>
+          </el-form-item>
+        </show-if-wrapper>
+      </template>
+    </el-form>
+    </div>
 </template>
 <script lang="ts" setup>
 import { reactive, ref } from "vue";
 import FieldItem from "./FieldItem.vue";
-import exampleForm from "../mocks/exampleForm.json";
+import creditCardForm from "../mocks/creditCardForm.json";
 import FieldCol from "./FieldCol.vue";
 import FieldSpan from "./fields/FieldSpan.vue";
 import type { Schema, FormConfig } from "../types/Schema";
@@ -48,26 +50,20 @@ const mapFunctionRule: MapFunctionRule = {
   "validateEmailWithForm": validateEmailWithForm,
 };
 const form = reactive<Record<string, any>>({
-  name: "Sample Value",
-  region: null,
-  email: "parnupat.j@gmail.com",
-  resource: "Venue",
-  type: [],
-  date1: "",
-  time1: null,
-  desc: null,
-  isActive: true,
-  isAccepted: false,
+  cardHolderName: "",
+  cardNumber: "",
+  expiryDate: "",
+  cvv: "",
 });
 
 const config = reactive<FormConfig>({
-  labelWidth: "120px",
+  labelWidth: "150px",
   labelPosition: "left",
   showMessage: true,
   inline: false,
   size: "default",
 })
-const fields = reactive<Schema>(exampleForm);
+const fields = reactive<Schema>(creditCardForm);
 const warpElForm = ref<InstanceType<
   typeof import("element-plus")["ElForm"]
 > | null>(null);
