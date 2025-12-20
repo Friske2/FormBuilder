52e88d2b19e917a8930ef0f8707be6081585793f
Friske2 <parnupat.j@gmail.com>
2025-12-20 14:13:21 +0700

    imp func install from npm publish

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCheckboxGroup: typeof import('element-plus/es')['ElCheckboxGroup']
     ElCol: typeof import('element-plus/es')['ElCol']
