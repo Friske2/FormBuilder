7119a56009d83e02f37eaa08439c1cbe89b2a250
Friske2 <parnupat.j@gmail.com>
2026-05-08 01:09:34 +0700

    fix: rename project from FormBuilder to Uniforms and update related configurations

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -26,8 +26,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "FormBuilder",
-      fileName: (format) => `form-builder.${format}.js`,
+      name: "Uniforms",
+      fileName: (format) => `uniforms.${format}.js`,
       formats: ["es", "umd"],
     },
     rollupOptions: {
